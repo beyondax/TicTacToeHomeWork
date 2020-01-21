e6ca00c45fd3d9bbe3c3ec16337dfa03a9d98c5b
beyondax <antonyxdark@gmail.com>
2020-01-21 16:58:56 +0300

    Without tests.

---
+++ TicTacToe-master/TicTacToeHomeWork/app/src/test/java/com/example/tictactoehomework/model/TicTacToeTests.java
@@ -0,0 +1,135 @@
+package com.example.tictactoehomework.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
+
+public class TicTacToeTests {
+
+    private Board board;
+
+    @Before
+    public void setup() {
+        board = new Board();
+    }
+
+    /**
+     * This test will simulate and verify X is the winner.
+     * <p>
+     * X | X | X
+     * O |   |
+     * | O |
+     */
+
+    @Test
+    public void test3inRowAcrossTopForX() {
+
+        board.mark(0, 0); // X
+        assertNull(board.getWinner());
+
+        board.mark(1, 0); // O
+        assertNull(board.getWinner());
+
+        board.mark(0, 1); // X
+        assertNull(board.getWinner());
+
+        board.mark(2, 1); // O
+        assertNull(board.getWinner());
+
+        board.mark(0, 2); // X
+        assertEquals(Player.X, board.getWinner());
+    }
+
+    /**
+     * This test will simulate and verify X is the winner.
+     * <p>
+     * O |   |
+     * X | X | X
+     * | O |
+     */
+
+    @Test
+    public void test3inRowMidLineFromLeftToRightForX() {
+
+        board.mark(1, 0); // X
+        assertNull(board.getWinner());
+
+        board.mark(0, 0); // O
+        assertNull(board.getWinner());
+
+        board.mark(1, 1); // X
+        assertNull(board.getWinner());
+
+        board.mark(2, 1); // O
+        assertNull(board.getWinner());
+
+        board.mark(1, 2); // X
+        assertEquals(Player.X, board.getWinner());
+    }
+
+
+    /**
+     * This test will simulate and verify O is the winner.
+     * <p>
+     * O | X | X
+     * | O |
+     * | X | O
+     */
+
+    @Test
+    public void test3inRowDiagonalFromTopLeftToBottomForO() {
+
+        board.mark(0, 1); // X
+        assertNull(board.getWinner());
+
+        board.mark(0, 0); // O
+        assertNull(board.getWinner());
+
+        board.mark(2, 1); // X
+        assertNull(board.getWinner());
+
+        board.mark(1, 1); // O
+        assertNull(board.getWinner());
+
+        board.mark(0, 2); // X
+        assertNull(board.getWinner());
+
+        board.mark(2, 2); // O
+        assertEquals(Player.O, board.getWinner());
+
+    }
+
+    /**
+     * This test will simulate and verify O is the winner.
+     * <p>
+     * X | O | X
+     * | O | X
+     * | O |
+     */
+
+    @Test
+    public void test3inMidLineFromTopToBottomForO() {
+
+        board.mark(0, 0); // X
+        assertNull(board.getWinner());
+
+        board.mark(0, 1); // O
+        assertNull(board.getWinner());
+
+        board.mark(0, 2); // X
+        assertNull(board.getWinner());
+
+        board.mark(1, 1); // O
+        assertNull(board.getWinner());
+
+        board.mark(1, 2); // X
+        assertNull(board.getWinner());
+
+        board.mark(2, 1); // O
+        assertEquals(Player.O, board.getWinner());
+
+    }
+
+}
---
+++ TicTacToe-master/TicTacToeHomeWork/app/src/test/java/com/example/tictactoehomework/presenter/TicTacToePresenterTests.java
@@ -0,0 +1,153 @@
+package com.example.tictactoehomework.presenter;
+
+
+import com.example.tictactoehomework.R;
+import com.example.tictactoehomework.view.TicTacView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * There are a lot more tests we can and should write but for now, just a few smoke tests.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TicTacToePresenterTests {
+
+    private TicTacPresenter presenter;
+
+    @Mock
+    private TicTacView view;
+
+    @Before
+    public void setup() {
+        presenter = new TicTacPresenter(view);
+    }
+
+    private void clickAndAssertValueAt(int row, int col, int expectedDrawableRes) {
+        presenter.onImagePressed(row, col);
+        verify(view).setImage(row, col, expectedDrawableRes);
+    }
+
+    /**
+     * This test will simulate and verify X is the winner.
+     * <p>
+     * X | X | X
+     * O |   |
+     * | O |
+     */
+    @Test
+    public void test3inRowAcrossTopForX() {
+
+        clickAndAssertValueAt(0, 0, R.drawable.cross);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(1, 0, R.drawable.circle);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(0, 1, R.drawable.cross);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(2, 1, R.drawable.circle);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(0, 2, R.drawable.cross);
+        verify(view).showWinner("X");
+
+    }
+
+    /**
+     * This test will simulate and verify X is the winner.
+     * <p>
+     * | O |
+     * X | X | X
+     * | O |
+     */
+    @Test
+    public void test3inRowAcrossMidForX() {
+
+        clickAndAssertValueAt(1, 0, R.drawable.cross);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(0, 1, R.drawable.circle);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(1, 1, R.drawable.cross);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(2, 1, R.drawable.circle);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(1, 2, R.drawable.cross);
+
+        verify(view).showWinner("X");
+
+    }
+
+    /**
+     * This test will simulate and verify X is the winner.
+     * <p>
+     * |   |
+     * O | O |
+     * X | X | X
+     */
+    @Test
+    public void test3inRowAcrossBotForX() {
+
+        clickAndAssertValueAt(2, 0, R.drawable.cross);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(0, 1, R.drawable.circle);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(2, 1, R.drawable.cross);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(1, 1, R.drawable.circle);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(2, 2, R.drawable.cross);
+
+        verify(view).showWinner("X");
+
+    }
+
+
+    /**
+     * This test will simulate and verify O is the winner.
+     * <p>
+     * O | X | X
+     * | O |
+     * | X | O
+     */
+    @Test
+    public void test3inRowDiagonalFromTopLeftToBottomForO() {
+
+        clickAndAssertValueAt(0, 1, R.drawable.cross);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(0, 0, R.drawable.circle);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(2, 1, R.drawable.cross);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(1, 1, R.drawable.circle);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(0, 2, R.drawable.cross);
+        verify(view, never()).showWinner(anyString());
+
+        clickAndAssertValueAt(2, 2, R.drawable.circle);
+        verify(view).showWinner("O");
+
+    }
+
+
+}
